3de20001e5c1ae860d1b2abc2c7e2287f9663fcb
Ivan Timofeev <43262229+alexrush1@users.noreply.github.com>
2021-12-28 02:44:39 +0700

    refs #4: правка по замечаниям

--- app/src/main/java/nsu/timofeev/badweatherapp/weather/current_weather_model/CityCurrentWeather.kt
+++ app/src/main/java/nsu/timofeev/badweatherapp/weather/current_weather_model/CityCurrentWeather.kt
@@ -1,24 +1,26 @@
 package nsu.timofeev.badweatherapp.weather.current_weather_model
 
-class CityCurrentWeather(
+import java.io.Serializable
+
+data class CityCurrentWeather (
 
         val main: Main,
         val name: String,
         val sys: Sys,
         val weather: List<Weather>
-)
+) : Serializable
 
 data class Main(
 
         val feels_like: Double,
         val humidity: Int,
         val temp: Double
-)
+) : Serializable
 
 data class Sys(
 
         val country: String
-)
+) : Serializable
 
 data class Weather(
 
@@ -26,4 +28,4 @@ data class Weather(
         val icon: String,
         val id: Int,
         val main: String
-)
+) : Serializable
--- app/src/main/java/nsu/timofeev/badweatherapp/weather/weather_forecast_model/CityWeatherForecast.kt
+++ app/src/main/java/nsu/timofeev/badweatherapp/weather/weather_forecast_model/CityWeatherForecast.kt
@@ -1,9 +1,11 @@
 package nsu.timofeev.badweatherapp.weather.weather_forecast_model
 
+import java.io.Serializable
+
 data class CityWeatherForecast(
 
     val list: List<Forecast>
-)
+) : Serializable
 
 data class Forecast(
 
@@ -11,16 +13,16 @@ data class Forecast(
     val dt_txt: String,
     val main: Main,
     val weather: List<Weather>
-)
+) : Serializable
 
 data class Main(
 
     val temp_max: Double,
     val temp_min: Double
-)
+) : Serializable
 
 data class Weather(
 
     val icon: String,
     val id: Int
-)
+) : Serializable
